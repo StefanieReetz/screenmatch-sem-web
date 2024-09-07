060f90d28f1795a41ccbd06685c54bf1432253e2
StefanieReetz <stefaniebeatrizreetz@gmail.com>
2024-09-07 17:30:46 -0300

    Melhorando o acesso na web com o VS Code

---
+++ src/main/java/br/com/alura/screenmatch2/dto/SerieDTO.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatch2.dto;
+
+import br.com.alura.screenmatch2.model.Categoria;
+
+public record SerieDTO(Long id,
+                       String titulo,
+                       Integer totalTemporadas,
+                       Double avaliacao,
+                       Categoria genero,
+                       String atores,
+                       String poster,
+                       String sinopse) {
+}
